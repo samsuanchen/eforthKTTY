421c41449327a6a03214daa7f61fdebd46dba959
sam <samsuanchen@gmail.com>
2014-03-07 17:02:33 +0800

    '1 EMIT' as the first output for hidden processing now ok

---
+++ components/eforthKTTY-statusbar/index20140307.jsx
@@ -0,0 +1,15 @@
+/** @jsx React.DOM */
+
+//var othercomponent=Require("other"); 
+var statusbar = React.createClass({
+  getInitialState: function() {
+  },
+  render: function() {
+    var s=this.props.hideText
+    s=s?s.replace(/</g,'&lt;'):'';
+    return (
+      <pre dangerouslySetInnerHTML={{__html:s}}/>
+    );
+  }
+});
+module.exports=statusbar;
---
+++ x.bat
@@ -0,0 +1,5 @@
+copy components\eforthKTTY-inputarea\index.jsx components\eforthKTTY-inputarea\index_.jsx
+copy components\eforthKTTY-main\index.jsx components\eforthKTTY-main\index_.jsx
+copy components\eforthKTTY-outputarea\index.jsx components\eforthKTTY-outputarea\index_7.jsx
+copy components\eforthKTTY-statusbar\index.jsx components\eforthKTTY-statusbar\index_.jsx
+copy components\eforthKTTY-titlebar\index.jsx components\eforthKTTY-titlebar\index_.jsx
